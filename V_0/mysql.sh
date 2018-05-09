6a3bd056cef3f3c661783dc7b6c3c0af9a553c91
Sayan <34585895+Sayan0Programmer@users.noreply.github.com>
2018-05-09 10:49:48 +0530

    Add files via upload

---
+++ V_0/mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo Installing MySQL
+echo Please Enter /"password/" as password
+#sudo apt-get install mysql-server
+echo Done
+echo "Please type $ mysql -u root -p[ENTER]"
+echo Enter password as password
+echo "Enter CREATE DATABASE RobBot;[ENTER]"
+echo "Enter "
+echo All Set...You are Good to Go
+#etc.
